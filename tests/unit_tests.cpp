f4561454eed79c6a8a96ebeacdfc8eee1d37c98b
Mackay Grange <mackay.grange@gmail.com>
2025-01-13 16:14:49 -0700

    Update unit_tests.cpp

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -4,17 +4,8 @@
 #include <sstream>
 #include <string>
 
-int get_input();
-
 void set_stdin(const std::string& input) 
 {
     std::istringstream* iss = new std::istringstream(input);
     std::cin.rdbuf(iss->rdbuf());
-}
-
-TEST(main_test, get_input_test_valid)
-{
-    std::string result = convTenTo2(255);
-    std::string expected = "11111111";
-    EXPECT_EQ(result, expected) << "== Incorrect Conversion of 255 to Binary =="
 }
